022bbab325544a667f777d4dec364b5d9f252b89
Mark Railton <marksrailton@gmail.com>
2021-06-15 21:25:50 +0100

    Adds login redirect

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', IndexController::class);
+Route::get('/login', function () {
+    return redirect('/admin/login');
+})->name('login');

1886492cd0981381fe8b09ee744eed4095a70124
Mark Railton <marksrailton@gmail.com>
2021-06-15 19:23:23 +0100

    Adds filament admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Concerns\IsFilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, IsFilamentUser;
 
     /**
      * The attributes that are mass assignable.

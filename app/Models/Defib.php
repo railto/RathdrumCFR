e3bd94d7f1ed7c6e6bc1e56c228591b3bc838b96
Mark Railton <marksrailton@gmail.com>
2021-06-15 21:15:34 +0100

    Adds audit logging

--- app/Models/Defib.php
+++ app/Models/Defib.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Defib extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     protected $guarded = [];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logAll();
+    }
 }

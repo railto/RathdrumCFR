4a15170fb7c9e13d8bdece7e1e3d28ea8fae061c
Mark Railton <marksrailton@gmail.com>
2021-07-01 19:32:00 +0100

    Add properties to models

--- app/Models/DefibNote.php
+++ app/Models/DefibNote.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Database\Factories\DefibNoteFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\DefibNote
+ *
+ * @property int $id
+ * @property int $defib_id
+ * @property int $user_id
+ * @property string $note
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Defib $defib
+ * @method static DefibNoteFactory factory(...$parameters)
+ * @method static Builder|DefibNote newModelQuery()
+ * @method static Builder|DefibNote newQuery()
+ * @method static Builder|DefibNote query()
+ * @method static Builder|DefibNote whereCreatedAt($value)
+ * @method static Builder|DefibNote whereDefibId($value)
+ * @method static Builder|DefibNote whereId($value)
+ * @method static Builder|DefibNote whereNote($value)
+ * @method static Builder|DefibNote whereUpdatedAt($value)
+ * @method static Builder|DefibNote whereUserId($value)
+ * @mixin Eloquent
+ */
 class DefibNote extends Model
 {
     use HasFactory;

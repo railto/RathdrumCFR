7efcf75dc1794c87311f6e5ab61ed97988f87601
Mark Railton <marksrailton@gmail.com>
2021-07-02 21:17:01 +0100

    Redesign the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,13 @@
-<footer class="mt-20">
-    <div class="container mx-auto px-6 py-4">
-        <div class="flex justify-between text-gray-600">
-            <div>
-                Website by <a href="https://markrailton.com" target="_blank">Mark Railton</a>
+<footer class="py-12 px-4">
+    <div aria-label="footer" class="focus:outline-none mx-auto container flex flex-col items-center justify-center">
+        <div class="w-9/12 h-0.5 bg-gray-100 rounded-full"></div>
+        <div class="text-black flex flex-col md:items-center f-f-l pt-3 text-center">
+            <div class="mt-6 mb-2 text-base text-color f-f-l">
+                &copy; Rathdrum Community First Responders {{ now()->format('Y') }}
             </div>
-
-            <div>
-                &copy; Rathdrum Community First Responders {{ now()->format('Y') }}</div>
-            <div>
-                Charity Number: 20204442
+            <div class="text-sm text-color mb-2 f-f-l">
+                <p class="focus:outline-none">Registered Charity Number: 20204442</p>
+                <p class="focus:outline-none">Website by <a href="https://markrailton.com" target="_blank">Mark Railton</a></p>
             </div>
         </div>
     </div>
